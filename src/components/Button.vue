0de5ae932b510fa2e5132470edeeda54d825e95a
Yura <yurii_kulachenko@mail.ru>
2021-03-25 12:53:41 +0300

    Add app

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    class="btn"
+    :style="{ background: color }"
+    @click="$emit('toggle-form')"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Created by Yura</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
